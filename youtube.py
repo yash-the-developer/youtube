7f824b85e10f228d77ad88f5a5ad0a7674701af0
yash-the-developer <87313121+yash-the-developer@users.noreply.github.com>
2021-10-04 13:11:46 +0530

    Add files via upload

---
+++ youtube.py
@@ -0,0 +1,21 @@
+import requests
+from flask import Flask
+from flask_cors import CORS, cross_origin
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return 'Api is working'
+
+@app.route('/<string:topic>')
+def playonyoutube(topic):
+    url='https://pywhatkit.herokuapp.com/playonyt?topic='+topic
+    request=requests.get(url)
+    return(request.text)
+
+if __name__ == '__main__':
+  app.run()
